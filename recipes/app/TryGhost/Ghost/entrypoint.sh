c20813bfe7e8f54f579a78bc029e98afa99ae06c
ben-ic <ben@interactivecats.com>
2026-08-21 22:43:26 +0300

    Fix Ghost database binding names

--- recipes/app/TryGhost/Ghost/entrypoint.sh
+++ recipes/app/TryGhost/Ghost/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -eu
 
-: "${GHOST_DB_HOST:?DropLive supplies the MySQL host}"
-: "${GHOST_DB_PORT:?DropLive supplies the MySQL port}"
-: "${GHOST_DB_USER:?DropLive supplies the MySQL user}"
-: "${GHOST_DB_PASSWORD:?DropLive supplies the MySQL password}"
-: "${GHOST_DB_DATABASE:?DropLive supplies the MySQL database}"
+: "${GHOST_CONNECTION_HOST:?DropLive supplies the MySQL host}"
+: "${GHOST_CONNECTION_PORT:?DropLive supplies the MySQL port}"
+: "${GHOST_CONNECTION_USER:?DropLive supplies the MySQL user}"
+: "${GHOST_CONNECTION_PASSWORD:?DropLive supplies the MySQL password}"
+: "${GHOST_CONNECTION_DATABASE:?DropLive supplies the MySQL database}"
 
 export database__client=mysql
-export database__connection__host="$GHOST_DB_HOST"
-export database__connection__port="$GHOST_DB_PORT"
-export database__connection__user="$GHOST_DB_USER"
-export database__connection__password="$GHOST_DB_PASSWORD"
-export database__connection__database="$GHOST_DB_DATABASE"
+export database__connection__host="$GHOST_CONNECTION_HOST"
+export database__connection__port="$GHOST_CONNECTION_PORT"
+export database__connection__user="$GHOST_CONNECTION_USER"
+export database__connection__password="$GHOST_CONNECTION_PASSWORD"
+export database__connection__database="$GHOST_CONNECTION_DATABASE"
 
 exec docker-entrypoint.sh "$@"
