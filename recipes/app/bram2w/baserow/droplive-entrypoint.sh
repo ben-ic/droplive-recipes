dea76d2a98d24e075fff4ddd9ad23078fe603496
ben-ic <ben@interactivecats.com>
2026-08-22 04:55:16 +0300

    Pin Baserow internal upstreams to IPv4

--- recipes/app/bram2w/baserow/droplive-entrypoint.sh
+++ recipes/app/bram2w/baserow/droplive-entrypoint.sh
@@ -6,6 +6,8 @@ set -eu
 
 exec env \
   BASEROW_PUBLIC_URL="$APP_URL" \
+  PRIVATE_BACKEND_URL="http://127.0.0.1:8000" \
+  PRIVATE_WEB_FRONTEND_URL="http://127.0.0.1:3000" \
   DATABASE_HOST="$(python3 -c 'from os import environ; from urllib.parse import urlsplit; print(urlsplit(environ["DATABASE_URL"]).hostname or "")')" \
   DATABASE_PORT="$(python3 -c 'from os import environ; from urllib.parse import urlsplit; print(urlsplit(environ["DATABASE_URL"]).port or 5432)')" \
   DATABASE_USER="$(python3 -c 'from os import environ; from urllib.parse import urlsplit, unquote; print(unquote(urlsplit(environ["DATABASE_URL"]).username or ""))')" \
