900282aebec514e18a71fa1b53d387d5a5c28c7f
ben-ic <ben@interactivecats.com>
2026-08-22 08:24:18 +0300

    Use recipe-owned Listmonk binding names

--- recipes/app/knadh/listmonk/entrypoint.sh
+++ recipes/app/knadh/listmonk/entrypoint.sh
@@ -3,11 +3,11 @@ set -eu
 
 # Listmonk uses mixed-case configuration names. DropLive binds portable
 # upper-case names, so map the reviewed companion values at the recipe edge.
-export LISTMONK_db__host="${DROPLIVE_LISTMONK_DB_HOST:?missing database host}"
-export LISTMONK_db__port="${DROPLIVE_LISTMONK_DB_PORT:?missing database port}"
-export LISTMONK_db__database="${DROPLIVE_LISTMONK_DB_DATABASE:?missing database name}"
-export LISTMONK_db__user="${DROPLIVE_LISTMONK_DB_USER:?missing database user}"
-export LISTMONK_db__password="${DROPLIVE_LISTMONK_DB_PASSWORD:?missing database password}"
+export LISTMONK_db__host="${LISTMONK_DB_HOST:?missing database host}"
+export LISTMONK_db__port="${LISTMONK_DB_PORT:?missing database port}"
+export LISTMONK_db__database="${LISTMONK_DB_DATABASE:?missing database name}"
+export LISTMONK_db__user="${LISTMONK_DB_USER:?missing database user}"
+export LISTMONK_db__password="${LISTMONK_DB_PASSWORD:?missing database password}"
 
 ./listmonk --install --idempotent --yes --config ''
 ./listmonk --upgrade --yes --config ''
